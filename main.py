2cbb54f9de99e2a52b7106a76cee3920f0652fdb
raccoonrocket <9802432172sergei@gmail.com>
2023-11-05 23:59:50 +0300

    add models

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import sessionmaker
 from config import db_data
-from models import Dealership
-from sqlalchemy import create_engine
+from models import (BrandCountry, CarBrand, ManufacturerCountry, CarSpecification, Model, Dealership,
+                    Category, Price, Description, Image, ModelCategory, ModelDealership, Review)
+from sqlalchemy import create_engine, MetaData
 
 engine = create_engine(db_data)
 engine.connect()
@@ -11,10 +12,22 @@ engine.connect()
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 db = SessionLocal()
 
-models = db.query(Dealership).all()
+# models = db.query(Model).all()
+#
+# for model in models:
+#     print(model.modelid, model.name, model.car_brand_id)
 
-for model in models:
-    print(model.dealershipid, model.name, model.address, model.phone, model.city)
+# metadata = MetaData()
+# metadata.reflect(bind=engine, schema='car_catalog')
+#
+# i = 0
+# # Print information about the tables and columns in the database
+# for table in metadata.tables.values():
+#     print("Table Name:", table.name)
+#     i += 1
+#     for column in table.c:
+#         print("Column Name:", column.name)
+# print("Total table = ", i)
 
 
 app = FastAPI()
--- models.py
+++ models.py
@@ -11,5 +11,53 @@ metadata = MetaData()
 metadata.reflect(bind=engine, schema='car_catalog')
 
 
+class BrandCountry(Base):
+    __table__ = Table('car_catalog.brand_country', metadata, autoload_with=engine)
+
+
+class CarBrand(Base):
+    __table__ = Table('car_catalog.carbrand', metadata, autoload_with=engine)
+
+
+class ManufacturerCountry(Base):
+    __table__ = Table('car_catalog.manufacturer_countries', metadata, autoload_with=engine)
+
+
+class CarSpecification(Base):
+    __table__ = Table('car_catalog.carspecifications', metadata, autoload_with=engine)
+
+
+class Model(Base):
+    __table__ = Table('car_catalog.models', metadata, autoload_with=engine)
+
+
 class Dealership(Base):
     __table__ = Table('car_catalog.dealerships', metadata, autoload_with=engine)
+
+
+class Category(Base):
+    __table__ = Table('car_catalog.categories', metadata, autoload_with=engine)
+
+
+class Price(Base):
+    __table__ = Table('car_catalog.prices', metadata, autoload_with=engine)
+
+
+class Description(Base):
+    __table__ = Table('car_catalog.description', metadata, autoload_with=engine)
+
+
+class Image(Base):
+    __table__ = Table('car_catalog.images', metadata, autoload_with=engine)
+
+
+class ModelCategory(Base):
+    __table__ = Table('car_catalog.modelcategories', metadata, autoload_with=engine)
+
+
+class ModelDealership(Base):
+    __table__ = Table('car_catalog.modeldealerships', metadata, autoload_with=engine)
+
+
+class Review(Base):
+    __table__ = Table('car_catalog.reviews', metadata, autoload_with=engine)
