e289c5aed32282dcef7ccf86979db331a45d4f2e
raccoonrocket <9802432172sergei@gmail.com>
2023-11-06 22:12:34 +0300

    models->database, add commits, brand-models view

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Body
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import sessionmaker
-from config import db_data
-from models import (BrandCountry, CarBrand, ManufacturerCountry, CarSpecification, Model, Dealership,
-                    Category, Price, Description, Image, ModelCategory, ModelDealership, Review)
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session, session
+from database import *
+from fastapi.responses import JSONResponse, FileResponse
 
-engine = create_engine(db_data)
-engine.connect()
+# app
+app = FastAPI()
 
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
+# connecting static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# defining the dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def main():
+    return FileResponse("templates/index.html")
+
+
+@app.get("/api/models")
+def get_models(db: Session = Depends(get_db)):
+    res = db.query(Model.modelid, CarBrand.car_brand_name, Model.name).join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).all()
+    processed_res = [(item[0], item[1], item[2]) for item in res1]
+    return processed_res
+    # return db.query(Model).all()
+
+
+# SessionLocal = sessionmaker(autoflush=False, bind=engine)
 db = SessionLocal()
 
-# models = db.query(Model).all()
-#
-# for model in models:
-#     print(model.modelid, model.name, model.car_brand_id)
+res1 = db.query(Model.modelid, CarBrand.car_brand_name, Model.name).join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).all()
+processed_res1 = [{item[0], item[1], item[2]} for item in res1]
+res2 = db.query(Model).all()
 
-# metadata = MetaData()
-# metadata.reflect(bind=engine, schema='car_catalog')
-#
-# i = 0
+for result in res1:
+    print(result)
+
 # # Print information about the tables and columns in the database
 # for table in metadata.tables.values():
 #     print("Table Name:", table.name)
-#     i += 1
 #     for column in table.c:
 #         print("Column Name:", column.name)
-# print("Total table = ", i)
-
-
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
