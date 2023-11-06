f7a59549bc0151db5a43a2ebd6ab7fbe5e4b0cb4
raccoonrocket <9802432172sergei@gmail.com>
2023-11-07 00:54:09 +0300

    adding description image, fix size images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, Body
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy.orm import Session, session
+from sqlalchemy.orm import Session
 from database import *
 from fastapi.responses import JSONResponse, FileResponse
 
@@ -27,9 +27,12 @@ def main():
 
 @app.get("/api/models")
 def get_models(db: Session = Depends(get_db)):
-    res = db.query(Model.modelid, CarBrand.car_brand_name, Model.name).join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).all()
-    processed_res = [(item[0], item[1], item[2]) for item in res1]
-    return processed_res
+    models = (db.query(Model.modelid, Model.name, CarBrand.car_brand_name, Description.description, Image.url).
+              join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).
+              join(Description, Model.modelid == Description.model_id).
+              join(Image, Model.modelid == Image.model_id).all())
+    processed_models = [(item[0], item[1], item[2], item[3], item[4]) for item in models]
+    return processed_models
     # return db.query(Model).all()
 
 
@@ -39,8 +42,12 @@ db = SessionLocal()
 res1 = db.query(Model.modelid, CarBrand.car_brand_name, Model.name).join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).all()
 processed_res1 = [{item[0], item[1], item[2]} for item in res1]
 res2 = db.query(Model).all()
-
-for result in res1:
+models = (db.query(Model.modelid, Model.name, CarBrand.car_brand_name, Description.description, Image.url).
+              join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).
+              join(Description, Model.modelid == Description.model_id).
+              join(Image, Model.modelid == Image.model_id).all())
+processed_models = [(item[0], item[1], item[2], item[3], item[4]) for item in models]
+for result in models:
     print(result)
 
 # # Print information about the tables and columns in the database
