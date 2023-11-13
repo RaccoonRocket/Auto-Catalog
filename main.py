0cac7942f0101c3b3fe61a13813f8287742b69ca
raccoonrocket <9802432172sergei@gmail.com>
2023-11-13 19:54:49 +0300

    remove unnecessary

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Depends, Body
+from fastapi import FastAPI, Depends
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from database import *
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi.responses import FileResponse
 
 # app
 app = FastAPI()
@@ -110,29 +110,3 @@ def about_brand(name, db: Session = Depends(get_db)):
              filter(CarBrand.car_brand_name == name).all())
     processed_brand = [(item[0], item[1]) for item in brand]
     return processed_brand
-
-
-
-# db = SessionLocal()
-# id = 15
-# # brands = ['Chery', 'Haval', 'Geely', 'Exeed', 'Changan', 'Zeekr']
-# # categories = ['Седан', 'Кроссовер', 'Хэтчбек', 'Универсал', 'Внедорожник', 'Купе', 'Кабриолет', 'Пикап', 'Минивэн']
-# # prices = ['0', '15000000', 'asc']
-# model = (db.query(Model.modelid, Image.url, Model.name, CarBrand.car_brand_name,
-#                   Category.name, Price.price, Description.description).
-#              join(Image, Model.modelid == Image.model_id).
-#              join(CarBrand, Model.car_brand_id == CarBrand.car_brand_id).
-#              join(ModelCategory, Model.modelid == ModelCategory.modelid).
-#              join(Category, ModelCategory.categoryid == Category.categoryid).
-#              join(Price, Model.modelid == Price.model_id).
-#              join(Description, Model.modelid == Description.model_id).
-#              filter(Model.modelid == id).all())
-# processed_model = [model]
-# # processed_model = [(item[0], item[1], item[2], item[3], item[4], item[5],
-# #                     item[6], item[7], item[8], item[9], item[10]) for item in model]
-# processed_model = [(item[0], item[1], item[2], item[3], item[4], item[5], item[6]) for item in model]
-# i = 0
-# for m in processed_model:
-#     print(m)
-#     i += 1
-# print(i)
